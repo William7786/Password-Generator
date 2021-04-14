43147e79a5e51366863881177fdaccfbb0632b5e
Tcomp <williamt7786@gmail.com>
2021-04-14 17:44:22 -0400

    This commit was for the basics of the password generator

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Password-Generator
+First I created the outline of the password generator so it can run with no errors.
+Then I added characters to the var password.
+Next I added a for statement.
+After that I created an small scrip to get element by id.
+//problems- it runs with no errors but in the password box it displays all of the characters given to generate password.
--- assets/script.js
+++ assets/script.js
@@ -3,11 +3,17 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var password = ("0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!@#$%^&*()[]{}<>;:-_=+~`");
+  var passwordLength = 15;
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
 
+  for (var i = 0; i< passwordLength; i++) {
+    var randomNum = Math.floor(Math.random() * passwordLength);
+    password += password.substring(randomNum ,randomNum+1, randomNum+2)
+    
+  }
+document.getElementById("password").value = password
 }
 
 // Add event listener to generate button
