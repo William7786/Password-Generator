f3c7175cc39098987397ad24dd9fa65de61f69ad
Tcomp <williamt7786@gmail.com>
2021-04-17 12:25:25 -0400

    this commit was for my alert boxes and prompt boxes

--- README.md
+++ README.md
@@ -5,6 +5,7 @@ Next I added a for statement.
 After that I created an small scrip to get element by id.  
 //problems- it runs with no errors but in the password box it displays all of the characters given to generate password.  
 Next I deleted unessasary code like the "var passwordText = document.  querySelector("#password")".   
-Then I added the variable Characters and added is to my for statement.  
-I also had to add value to the password variable to fix the issue i had earlier with the password display box.    
+Then I added the variable Characters and added it to my for statement.  
+I also had to add value to the password variable to fix the issue I had earlier with the password display box.
+I created a New alert box and all the prompts I need.
 
--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,35 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
+var Uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var Lowercase = "abcdefghijklmnopqrstuvwxyz";
+var Numbers = "0123456789";
+var Special = "!@#$%^&*()[]{}<>;:-_=+~`";
 // Write password to the #password input
-function writePassword() {
-  var characters = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!@#$%^&*()[]{}<>;:-_=+~`";
-  var password = "Password: ";
-  var passwordLength = 15;
+alert("Welcome to my password generator! Lets create your password!")
+let Upper= prompt("Would you like UPPERCASE letters? Yes/No")
+let Lower= prompt("Would you like Lowercase letters? Yes/No")
+let Number= prompt("Would you like Numbers? Yes/No")
+let SpecialChar= prompt("Would you like Special Characters? Yes/No")
+let Length= prompt("How many characters long would you like your password to be? 8-118")
 
-  for (var i = 0; i< passwordLength; i++) {
-    var randomNum = Math.floor(Math.random() * characters.length);
-    password += characters.substring(randomNum, randomNum+1, randomNum+2)
+function Upper(){
+  
+}
+
+function writePassword() {
+  var characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz123456789!@#$%^&*()[]{}<>;:-_=+~`";
+  var password = "Password: ";
+  var charactersLength = "78";
+
+
+
+  for (var i = 0; i< charactersLength; i++) {
+    var randomNum = Math.floor(Math.random() * charactersLength);
+    password += characters.substring(randomNum, randomNum+1,)
     
   }
+ 
+  
 document.getElementById("password").value = password
 }
 
